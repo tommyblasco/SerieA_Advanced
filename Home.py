78f155026c7d8b23dc69c06e8dc9bc23af11c84a
me <sacchinit@gmail.com>
2025-03-08 16:32:20 +0100

    prova iniziale

---
+++ Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import understatapi
+
+st.set_page_config(page_title="Serie A",layout='wide')
+
+st.title("Serie A ai raggi X(G)")
+
+st.subheader("Statistiche avanzate sulla moderna Serie A")
+
+
