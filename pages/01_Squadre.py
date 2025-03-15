f42dec337429211dd09b03bea101c19860b672c2
me <sacchinit@gmail.com>
2025-03-15 15:49:11 +0100

    tiri

--- pages/01_Squadre.py
+++ pages/01_Squadre.py
@@ -92,7 +92,7 @@ with att_sk:
             st.write('Distanza media tiri')
             dm_gr = go.Figure(data=[go.Bar(
                     x=df_sh['Standard_Dist.'], y=df_sh['Squadra'],
-                    width=0.1,  marker=dict(size=12, symbol='circle-x', color='orange', line=dict(color='orange', width=1)))
+                    width=0.1,  marker=dict(symbol='circle-x', color='orange', line=dict(color='orange', width=1)))
             ])
             dm_gr.update_layout( xaxis=dict(side='top'), plot_bgcolor='green', paper_bgcolor='green')
             st.plotly_chart(go.FigureWidget(data=dm_gr), use_container_width=True)
