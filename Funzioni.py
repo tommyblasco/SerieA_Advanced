42974ac955617eb227bd7515f748b20153360d87
me <sacchinit@gmail.com>
2025-03-16 10:11:10 +0100

    add all logos

--- Funzioni.py
+++ Funzioni.py
@@ -4,7 +4,9 @@ import requests
 from PIL import Image
 from io import BytesIO
 import plotly.graph_objects as go
-from Home import sito_fbref, sel_stag, sel_camp, dict_camp
+from Home import dict_camp
+
+sito_fbref=f'https://fbref.com/it/comps/{dict_camp[st.session_state['camp']]}/{st.session_state['camp'].replace(' ','-')}-Stats'
 
 def update_colnames(col_list):
     n=len(col_list)
@@ -15,10 +17,10 @@ def update_colnames(col_list):
         else:
             new_col.append(col_list[i][0]+"_"+col_list[i][1])
     return new_col
-def get_stats_fbref(table,s=sito_fbref,stag=sel_stag):
+def get_stats_fbref(table,s=sito_fbref,stag=st.session_state['stag']):
     df=pd.read_html(s,attrs={'id':table})[0]
-    if table!=f'results{stag}{dict_camp[sel_camp]}1_overall':
+    if table!=f'results{stag}{dict_camp[st.session_state['camp']]}1_overall':
         df.columns=update_colnames(df.columns)
-    rad_sito=f"https://raw.githubusercontent.com/tommyblasco/SerieA_Advanced/master/images/stemmi/{sel_camp.replace(' ','-')}/"
+    rad_sito=f"https://raw.githubusercontent.com/tommyblasco/SerieA_Advanced/master/images/stemmi/{st.session_state['camp'].replace(' ','-')}/"
     df['link_img']=[rad_sito+x+'.png' for x in df['Squadra']]
     return df
--- Home.py
+++ Home.py
@@ -11,12 +11,23 @@ st.subheader("Statistiche avanzate sul calcio moderno europeo")
 home_img=Image.open(BytesIO(requests.get(f"https://github.com/tommyblasco/SerieA_Advanced/blob/master/images/varie/home.png?raw=true").content))
 st.image(home_img, use_container_width=True)
 
+st.sidebar.title("Sezione analisi")
+pagina = st.sidebar.radio("Vai a:", ["Squadre", "Giocatori", "Partite"])
 
 stagione_corso='2024-2025'
 
 list_stag2pass=[str(int(stagione_corso[:4])-1)+'-'+stagione_corso[:4], str(int(stagione_corso[:4])-2)+'-'+str(int(stagione_corso[:4])-1)]
 dict_camp = {'Premier League':'9','La Liga':'12','Serie A':'11','Bundesliga':'20','Ligue 1':'13'}
-sel_camp=st.sidebar.selectbox('Scegli un campionato:',dict_camp.keys())
-sel_stag=st.sidebar.selectbox('Scegli una stagione:',[stagione_corso]+list_stag2pass)
 
-sito_fbref=f'https://fbref.com/it/comps/{dict_camp[sel_camp]}/{sel_camp.replace(' ','-')}-Stats'
+st.sidebar.title("Filtra per:")
+st.session_state['camp']=st.sidebar.selectbox('Scegli un campionato:',dict_camp.keys())
+st.session_state['stag']=st.sidebar.selectbox('Scegli una stagione:',[stagione_corso]+list_stag2pass)
+
+if pagina == "Squadre":
+    exec(open("pages/01_Squadre.py").read())
+elif pagina == "Giocatori":
+    exec(open("pages/02_Giocatori.py").read())
+elif pagina == "Partite":
+    exec(open("pages/03_Partite.py").read())
+
+
