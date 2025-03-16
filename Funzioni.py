0494d17f390111c24a870fbe6c5c54d7549d2155
me <sacchinit@gmail.com>
2025-03-16 12:29:25 +0100

    add all logos

--- Funzioni.py
+++ Funzioni.py
@@ -12,9 +12,8 @@ def update_colnames(col_list):
             new_col.append(col_list[i][0]+"_"+col_list[i][1])
     return new_col
 
-stagion = st.session_state.get('stag', 'Serie A')
-league = st.session_state.get('camp', shared.stagione_corso)
-def get_stats_fbref(table,stag=stagion):
+def get_stats_fbref(table,stag=st.session_state.get('stag', 'Serie A')):
+    league = st.session_state.get('camp', shared.stagione_corso)
     s = f'https://fbref.com/it/comps/{shared.dict_camp[league]}/{league.replace(' ', '-')}-Stats'
     df=pd.read_html(s,attrs={'id':table})[0]
     if table!=f'results{stag}{shared.dict_camp[league]}1_overall':
