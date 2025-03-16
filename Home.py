08528f300aa5889f397edc9433c9eb5a7ce1c75a
me <sacchinit@gmail.com>
2025-03-16 10:13:25 +0100

    add all logos

--- Home.py
+++ Home.py
@@ -19,6 +19,11 @@ stagione_corso='2024-2025'
 list_stag2pass=[str(int(stagione_corso[:4])-1)+'-'+stagione_corso[:4], str(int(stagione_corso[:4])-2)+'-'+str(int(stagione_corso[:4])-1)]
 dict_camp = {'Premier League':'9','La Liga':'12','Serie A':'11','Bundesliga':'20','Ligue 1':'13'}
 
+if 'camp' not in st.session_state:
+    st.session_state['camp'] = "Serie A"
+if 'stag' not in st.session_state:
+    st.session_state['stag'] = stagione_corso
+
 st.sidebar.title("Filtra per:")
 st.session_state['camp']=st.sidebar.selectbox('Scegli un campionato:',dict_camp.keys())
 st.session_state['stag']=st.sidebar.selectbox('Scegli una stagione:',[stagione_corso]+list_stag2pass)
