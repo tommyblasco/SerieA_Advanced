ccb0acb4c9b8e392d678af488c5cc04307fe08d3
me <sacchinit@gmail.com>
2025-03-16 13:27:48 +0100

    add misc

--- Funzioni.py
+++ Funzioni.py
@@ -19,7 +19,7 @@ def get_stats_fbref(table,stag,league):
     if stag!=stagione_corso:
         s = f'https://fbref.com/it/comps/{stag}/{dict_camp[league]}/{league.replace(' ', '-')}-Stats'
     else:
-        s = f'https://fbref.com/it/comps/{dict_camp[league]}/{league.replace(' ', '-')}-Stats'
+        s = f'https://fbref.com/it/comp/{dict_camp[league]}/{league.replace(' ', '-')}-Stats'
     df=pd.read_html(s,attrs={'id':table})[0]
     if table!=f'results{stag}{dict_camp[league]}1_overall':
         df.columns=update_colnames(df.columns)
